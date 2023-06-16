561e722bca8d151db6d1af89f2aec9ad13ddb902
Fatih CAKIR <126199168+fatihcckr@users.noreply.github.com>
2023-06-16 10:24:47 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,6 +20,6 @@ variable "server-name" {
 variable "docker-instance-ports" {
   type        = list(number)
   description = "docker-instance-sec-gr-inbound-rules"
-  default     = [22, 80, 8080]
+  default     = [22, 80, 443, 8080]
 }
 
